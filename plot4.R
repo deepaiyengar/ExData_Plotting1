953f0e577f383a4ce9e665b8be6b9719b4b7556a
deepaiyengar <deepa.iyengar@gmail.com>
2015-03-08 16:32:56 -0700

    plot1 file

---
+++ plot4.R
@@ -0,0 +1,22 @@
+## after downloading the zip file by clicking on it:
+  
+unzip("exdata-data-household_power_consumption.zip")
+install.packages("read.table")
+
+data <- read.table("household_power_consumption.txt",sep=";",dec=".",header=TRUE,stringsAsFactors=FALSE,na.strings="?", colClasses=c(rep("character",2), rep("numeric",7)))
+
+## catenate the days column to the dataset
+newdata <- subset(data, Date=="1/2/2007" | Date=="2/2/2007")
+
+date_data <- as.Date(newdata$Date,'%d/%m/%Y')
+
+data1 <- cbind(newdata, date_data)
+
+## plot 3
+  
+with(data1, plot(date_data, Sub_metering_1, type ="l", xlab="",ylab="Energy sub metering"))
+with(data1, plot(date_data, Sub_metering_2, type ="l", xlab="",ylab="Energy sub metering"))
+with(data1, plot(date_data, Sub_metering_3, type ="l", xlab="",ylab="Energy sub metering"))
+
+dev.copy(png,filename="plot3.png",width=480,height=480)
+dev.off(which = dev.cur())
