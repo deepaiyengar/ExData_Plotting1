0566be235f42004b5cee54bb542d28b8ac738043
deepaiyengar <deepa.iyengar@gmail.com>
2015-03-08 22:06:56 -0700

    plot1 file

--- plot3.R
+++ plot3.R
@@ -2,21 +2,25 @@
   
 unzip("exdata-data-household_power_consumption.zip")
 install.packages("read.table")
+install.packages("lubridate")
+library(lubridate)
 
 data <- read.table("household_power_consumption.txt",sep=";",dec=".",header=TRUE,stringsAsFactors=FALSE,na.strings="?", colClasses=c(rep("character",2), rep("numeric",7)))
 
 ## catenate the days column to the dataset
 newdata <- subset(data, Date=="1/2/2007" | Date=="2/2/2007")
 
-date_data <- as.Date(newdata$Date,'%d/%m/%Y')
+mydate = dmy(newdata$Date) + hms(newdata$Time)
 
-data1 <- cbind(newdata, date_data)
+data1 <- cbind(newdata, mydate)
 
 ## plot 3
-  
-with(data1, plot(date_data, Sub_metering_1, type ="l", xlab="",ylab="Energy sub metering"))
-with(data1, plot(date_data, Sub_metering_2, type ="l", xlab="",ylab="Energy sub metering"))
-with(data1, plot(date_data, Sub_metering_3, type ="l", xlab="",ylab="Energy sub metering"))
+png(filename = "plot3.png",width = 480, height = 480)  
+with(data1, plot(mydate, Sub_metering_1, type ="l", xlab="",ylab="Energy sub metering"))
+with(data1, lines(mydate, Sub_metering_2, col = "red"))
+with(data1, lines(mydate, Sub_metering_3, col = "blue"))
 
-dev.copy(png,filename="plot3.png",width=480,height=480)
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=1, col = c("black","red","blue"))
+
+## dev.copy(png,filename="plot3.png",width=480,height=480)
 dev.off(which = dev.cur())
