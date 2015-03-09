4b683e2017503f20de64e9899f411a7a5c29d5c7
deepaiyengar <deepa.iyengar@gmail.com>
2015-03-08 21:25:42 -0700

    plot1 file

--- plot2.R
+++ plot2.R
@@ -2,18 +2,20 @@
   
 unzip("exdata-data-household_power_consumption.zip")
 install.packages("read.table")
+install.packages("lubridate")
+library(lubridate)
 
 data <- read.table("household_power_consumption.txt",sep=";",dec=".",header=TRUE,stringsAsFactors=FALSE,na.strings="?", colClasses=c(rep("character",2), rep("numeric",7)))
 
 ## catenate the days column to the dataset
 newdata <- subset(data, Date=="1/2/2007" | Date=="2/2/2007")
 
-date_data <- as.Date(newdata$Date,'%d/%m/%Y')
+mydate = dmy(newdata$Date) + hms(newdata$Time)
 
-data1 <- cbind(newdata, date_data)
+data1 <- cbind(newdata, mydate)
 
 ## plot 2
   
-with(data1, plot(date_data, Global_active_power, type ="l", xlab="",ylab="Global Active Power (kilowatts)"))
-dev.copy(png,filename="plot1.png",width=480,height=480)
+with(data1, plot(mydate, Global_active_power, type ="l", xlab="",ylab="Global Active Power (kilowatts)"))
+dev.copy(png,filename="plot2.png",width=480,height=480)
 dev.off(which = dev.cur())
